6d5f9871a5d80355e22885574c66edcda87ee285
Vanessa <suede210810@gmail.com>
2024-05-30 15:14:37 +0200

    add user infos + restaurants favoris card

---
+++ profile_page.js
@@ -0,0 +1,26 @@
+const userInformations = 
+{
+  name: "Doe",
+  firstName: "John",
+  Campus: "Lyon",
+  Adress: "3 Avenue des Lilas",
+  city: "69002 Lyon",
+  tel: "06.53.62.56.35"
+}
+
+const restaurants = [
+  {
+    name: 'Le Petit Bistro',
+    image: 'https://source.unsplash.com/1600x900/?bistro,restaurant'
+  },
+  {
+    name: 'Pizza Pronto',
+    image: 'https://source.unsplash.com/1600x900/?pizza,italian'
+  },
+  {
+    name: 'Sushi Express',
+    image: 'https://source.unsplash.com/1600x900/?sushi,japanese'
+  }
+];
+
+const cardList = restaurants.map((restaurant) => element) 
