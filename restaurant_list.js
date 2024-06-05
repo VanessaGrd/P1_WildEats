3260d87d72b86731168c531d17b1f07ef58ba3c5
anisabidi <anisabidi83@gmail.com>
2024-06-05 12:00:21 +0200

    add restaurant list

---
+++ restaurant_list.js
@@ -0,0 +1,50 @@
+const logos = {
+    "French": "./assets/star.svg",
+    "Italian": "./assets/star.svg",
+    "Japanese": "./assets/star.svg",
+    "Vegetarian": "./assets/star.svg",
+    "Burger": "./assets/star.svg",
+  };
+  
+  function generateStars(count) {
+    let starsHtml = "";
+    for (let i = 0; i < count; i++) {
+      starsHtml += '<img src="./assets/star.svg" alt="star">';
+    }
+    return starsHtml;
+  }
+  
+  
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    const restaurantListContainer = document.getElementById("restaurant_list");
+  
+    fetch('./data/restaurants.json')
+      .then(response => response.json())
+      .then(data => {
+        const proposals = data.filter(restaurant => !restaurant.favorite);
+        console.log('Proposals:', proposals);
+        proposals.forEach(restaurant => {
+          restaurantListContainer.innerHTML += createRestaurantCard(restaurant);
+        });
+      })
+      .catch(error => console.error('Error fetching restaurant data:', error));
+  });
+
+  function createRestaurantCard(restaurant) {
+    return 
+      <a href="restaurant_detail.html?id=${restaurant.id}" class="restaurant_card">
+        <img class="restaurant_picture" src="${restaurant.image}" alt="${restaurant.name}"/>
+        <div class="title">
+          <div class="stars_like">
+            <div class="stars_block">
+              <h3>${restaurant.name}</h3>
+              <div>${generateStars(restaurant.stars)}</div>
+            </div>
+          </div>
+          <p>${restaurant.address}</p>
+          <img class="restaurant_type" src="${logos[restaurant.type]}" alt="${restaurant.type}"/>
+        </div>
+      </a>
+    ;
+  }
