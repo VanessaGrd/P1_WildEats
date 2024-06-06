24c886836a885a7e779a42a3ae98060e43c5b0a0
Alicia LACOTE <alicia.lacote@laposte.net>
2024-06-06 11:27:41 +0200

    ajout footer.css

--- connexion.js
+++ connexion.js
@@ -1,20 +1,32 @@
 const close = document.querySelector('.close');
 const connexionForm = document.querySelector('.connexionForm');
-
+const inscriptionForm = document.querySelector('.inscriptionForm');
+const close2 = document.querySelector('.close2');
+const connexionBtn = document.querySelector('.B2');
+const inscriptionBtn = document.querySelector('.B3');
 
 close.addEventListener("click", function() {
-    form.style.display = "none";
+    connexionForm.style.display = "none";
     connexionBtn.style.display = "flex"
 });
 
-const connexionBtn = document.querySelector('.B2');
+
 
 connexionBtn.addEventListener('click', function() {
 
-    form.style.display = "flex";
+    connexionForm.style.display = "flex";
     connexionBtn.style.display = "none"
 });
 
+close2.addEventListener("click", function() {
+    inscriptionForm.style.display = "none";
+    inscriptionBtn.style.display = "flex"
+});
 
+inscriptionBtn.addEventListener('click', function() {
+
+    inscriptionForm.style.display = "flex";
+    inscriptionBtn.style.display = "none"
+});
 
 
