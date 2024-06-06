65613afa199f1f73be80179532eaee39f915bbf0
Vanessa <suede210810@gmail.com>
2024-06-06 11:09:17 +0200

    add favorites

--- profile_page.js
+++ profile_page.js
@@ -1,3 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const restaurantListContainer = document.getElementById("favorites_list");
+  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+  if (favorites.length === 0) {
+    restaurantListContainer.innerHTML = '<p>Aucun favori trouvé.</p>';
+    return;
+  }
+
+  fetch('./data/restaurants.json')
+    .then(response => response.json())
+    .then(data => {
+      const favoriteRestaurants = data.filter(restaurant => favorites.includes(restaurant.id));
+      console.log('Favorite Restaurants:', favoriteRestaurants);
+
+      favoriteRestaurants.forEach(restaurant => {
+        restaurantListContainer.innerHTML += createRestaurantCard(restaurant);
+      });
+    })
+    .catch(error => console.error('Error fetching restaurant data:', error));
+});
+
 function generateStars(count) {
   let starsHtml = "";
   for (let i = 0; i < count; i++) {
@@ -6,35 +28,19 @@ function generateStars(count) {
   return starsHtml;
 }
 
-function createFavoriteCard(restaurant) {
+function createRestaurantCard(restaurant) {
   return `
-  <a href="restaurant_detail.html?id=${restaurant.id}" class="restaurant_card">
-  <img class="restaurant_picture" src="${restaurant.images[0]}" alt="${restaurant.name}"/>
-  <div class="title">
+    <a href="restaurant_detail.html?id=${restaurant.id}" class="restaurant_card">
+    <img class="restaurant_picture" src="${restaurant.images[0]}" alt="${restaurant.name}"/>
+    <div class="title">
         <div class="stars_like">
           <div class="stars_block">
             <h3>${restaurant.name}</h3>
             <div>${generateStars(restaurant.stars)}</div>
           </div>
-          <img class="favoris" src="./assets/heart.svg" alt="favoris">
         </div>
         <p>${restaurant.address}</p>
       </div>
     </a>
   `;
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  const favoriteListContainer = document.getElementById("favorite_list");
-
-  fetch('./data/restaurants.json')
-    .then(response => response.json())
-    .then(data => {
-      const favorites = data.filter(restaurant => restaurant.favorite);
-      console.log('Favorites:', favorites);
-      favorites.forEach(restaurant => {
-        favoriteListContainer.innerHTML += createFavoriteCard(restaurant);
-      });
-    })
-    .catch(error => console.error('Error fetching restaurant data:', error));
-});
